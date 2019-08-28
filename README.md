090c1e8212a94f80069ba4053efab8e1ebddac14
Luiz Paulo Godoy <luizpaulo@bleu.com.br>
2019-08-28 14:15:03 -0300

    Changed error structure

---
+++ README.md
@@ -0,0 +1,22 @@
+# Error codes for Bleu API
+
+This is the error JSON:
+```javascript
+{
+  "code": "1102",
+  "message": "Invalid 2FA code."
+}
+```
+## 10xx - Server or Network error
+#### 1000 UNKNOWN
+ * Unknown request error.
+
+#### 1001 DISCONNECTED
+ * Internal error. Please try again.
+
+## 11xx - Request issues
+#### 1100 UNKNOWN PARAMETERS
+ * An unknown parameter was sent.
+ 
+#### 1101 INVALID COUNTRY
+ * An unknown parameter was sent.
--- en-us.go
+++ en-us.go
@@ -2,32 +2,29 @@ package errorscode
 
 // ErrorStruct - Error Default Struct
 type ErrorStruct struct {
-	Code    string `json:"code"`
-	Message string `json:"message"`
+	Code     string `json:"code"`
+	HTTPCode int    `json:"http_code"`
+	Message  string `json:"message"`
 }
 
 // GetError - Returns a Default Error Struct with code and message
-func GetError(code string) (result ErrorStruct) {
-	result.Code = code
-	result.Message = Errors[code]
-	return
+func GetError(code string) ErrorStruct {
+	return Errors[code]
 }
 
 // Errors - Store errors code and message
-var Errors = map[string]string{
+var Errors = map[string]ErrorStruct{
 	// 10xx - General Server or Network issues
-	"1000": "Unknown request error.",
-	"1001": "Internal error. Please try again.",
+	"1000": ErrorStruct{"1000", 502, "Unknown request error."},
+	"1001": ErrorStruct{"1001", 500, "Internal error. Please try again."},
 
 	// 11xx - Request issues
-	"1100": "An unknown parameter was sent.",
-	"1101": "Invalid country.",
-	"1102": "Invalid 2FA code.",
-	"1103": "Invalid e-mail auth code.",
-	"1104": "CPF already exists.",
-	"1105": "Username already exists.",
-	"1106": "Insert 2FA code.",
-	"1107": "Insert e-mail code.",
-	"1108": "Invalid Login.",
-	"1109": "Too many attempts.",
+	"1100": ErrorStruct{"1100", 422, "An unknown parameter was sent."},
+	"1101": ErrorStruct{"1101", 403, "Invalid country."},
+	"1102": ErrorStruct{"1102", 401, "Invalid 2FA code."},
+	"1103": ErrorStruct{"1103", 401, "Invalid e-mail 2FA code."},
+	"1104": ErrorStruct{"1104", 409, "CPF already exists."},
+	"1105": ErrorStruct{"1105", 409, "Username already exists."},
+	"1106": ErrorStruct{"1106", 401, "Invalid Login."},
+	"1109": ErrorStruct{"1109", 429, "Too many attempts"},
 }
