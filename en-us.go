7ade34b7549e7d6a3847363fc2d53fbdc91607e4
Luiz Paulo Godoy <luizpaulo@bleu.com.br>
2019-08-28 09:44:18 -0300

    Create errors list

---
+++ en-us.go
@@ -0,0 +1,15 @@
+package errorscode
+
+var Errors = map[string]string{
+	// 10xx - General Server or Network issues
+	"1000": "An unknown error occured while processing the request.",
+	"1001": "Internal error; unable to process your request. Please try again.",
+
+	// 11xx - Request issues
+	"1100": "An unknown parameter was sent.",
+	"1101": "Invalid country.",
+	"1102": "Invalid 2FA code.",
+	"1103": "Invalid e-mail auth code.",
+	"1104": "CPF already exists.",
+	"1105": "Username already exists.",
+}
